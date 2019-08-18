2953d2abfe7c92da29ec18a27c8070a3630d06f7
vladblkvs <gremlinux@gmail.com>
2019-08-19 00:45:49 +0300

    Вносит мелкие правки в некоторые иконки, подключает шрифты второму слайдеру

--- gulpfile.js
+++ gulpfile.js
@@ -103,7 +103,7 @@ gulp.task("server", function() {
   });
 
   gulp.watch("source/sass/**/*.scss", gulp.series("css", "refresh"));
-  gulp.watch("source/img/icon-*svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/img/*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
 });
