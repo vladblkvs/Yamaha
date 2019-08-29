74c6b6fcb33b9a679d3a1dbce5971051fbdf24dc
vladblkvs <gremlinux@gmail.com>
2019-08-29 16:19:49 +0300

    Намечает адаптивность меню

--- source/js/script.js
+++ source/js/script.js
@@ -5,16 +5,16 @@ var specsItems = document.querySelectorAll('.specs__item');
 var sliderToggles = document.querySelectorAll('.specs__btn');
 
 var toggleSlide = function () {
- sliderToggles.forEach(function (toggle, i) {
-   toggle.addEventListener('click', function (evt) {
-     evt.preventDefault();
-     for (var j = 0; j < specsItems.length; j++) {
-       specsItems[j].classList.remove('specs__item--active');
-       slides[j].classList.remove('specs-slide--active');
-     }
-     specsItems[i].classList.add('specs__item--active');
-     slides[i].classList.add('specs-slide--active');
-   });
- });
+  sliderToggles.forEach(function (toggle, i) {
+    toggle.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      for (var j = 0; j < specsItems.length; j++) {
+        specsItems[j].classList.remove('specs__item--active');
+        slides[j].classList.remove('specs-slide--active');
+      }
+      specsItems[i].classList.add('specs__item--active');
+      slides[i].classList.add('specs-slide--active');
+    });
+  });
 };
 toggleSlide();
